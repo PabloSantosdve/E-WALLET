9d5ef883a77104423311171d0a84569591678427
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-04-07 13:03:24 -0300

    update Js

--- cliente.js
+++ cliente.js
@@ -9,24 +9,33 @@ function exibirCartoes()
 
 function moverCartao(id)
 {
-    let principalImg = document.getElementById('principal')
-    let cartaoImg = document.getElementById(id)
+    let principalImg = document.getElementById('principal') // Variavél que armazena todo o elemento do cartão principal (cartão maior da página)
+    let cartaoImg = document.getElementById(id) // Variavel que armazena o cartão que o usuário clicou
+    let valorFatura = document.getElementById('fatura1') // variavel que armazena o elemento que exibe o valor da fatura
 
-    let valor = document.getElementById('fatura1')
-    valor.innerText= "R$ 1500,64" 
+    valorFatura.innerText= "R$ 1500,64" 
 
-    let valor1 = document.getElementsByClassName('fatura2')
-    valor.innerText= "R$ 2000,00"
-
-    let valor2 = document.getElementsByClassName('fatura3')
-    valor.innerText= "R$ 900,00"
-
-
-
+    // Esse swicth vai verificar em qual cartão o usuário clicou e atribuir um valor de fatura diferente para cada cartão
+    switch(id)
+    {
+        // Caso: Se ID == 'cartão-tal", o valor da fatura vai ser x
+        case "cartao-nubank":
+            valorFatura.innerText= "R$ 800,00"
+            break
+        case "cartao-bradesco":
+            valorFatura.innerText= "R$ 500,40"
+            break
+        case "cartao-verde":
+            valorFatura.innerText= "R$ 238,60"
+            break
+        case "cartao-amarelo":
+            valorFatura.innerText= "R$ 1015,22"
+            break
+    }
+    
+    // O cartão que está como principal, tem seu elemento com display none, quando trocar o cartão principal precisa exibir o antigo e esconder o novo cartão escolhido
     exibirCartoes()
 
-    cartaoImg.style.display = "none"
-
-
-    principalImg.src = cartaoImg.src
+    cartaoImg.style.display = "none" // escondendo o elemento do cartão que o usuário clicou
+    principalImg.src = cartaoImg.src // Trocando o caminho das imagens do elemento principal para "trocar o cartão" de lugar
 }
